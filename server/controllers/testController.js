2c7f9a9653d613a643b131cf0e6d22f59a1cec5e
= <jacex17@gmail.com>
2019-12-20 17:27:37 -0500

    remove test controller

--- server/controllers/testController.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports = {
-    sayHi(req, res){
-        console.log("HELLO EVERYONE!")
-        res.json({data: "Hey my friend"})
-    }
-    }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const testController = require('../controllers/testController')
 const userController = require('../controllers/userController')
 
-router.get('/sayHi', testController.sayHi)
 router.post('/signup', userController.signUp)
 router.post('/login', userController.logIn)
 router.get('/loggedin', userController.loggedIn)
