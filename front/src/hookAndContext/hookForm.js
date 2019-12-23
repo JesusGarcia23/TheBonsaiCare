39d03bf7ceec43eff2bba99f0dbd7ac1007da444
= <jacex17@gmail.com>
2019-12-22 23:57:22 -0500

    Add hooks form handler file

---
+++ front/src/hookAndContext/hookForm.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+const useForm = (callback) => {
+const [inputs, setInputs] = useState({})
+
+const handleSubmit = (e, data, props) => {
+    e.preventDefault()
+    callback(data, props)
+}
+
+const hadleInputChange = (event) => {
+    event.persist()
+    setInputs(inputs => ({...inputs, [event.target.name]: event.target.value }));
+}
+return {
+    handleSubmit,
+    handleInputChange,
+    inputs, 
+    setInputs
+}
+}
+
+export default useForm;
