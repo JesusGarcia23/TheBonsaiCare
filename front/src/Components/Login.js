57e95e876d3f054fe363b5c2920986b6f8359ee2
= <jacex17@gmail.com>
2019-12-23 00:25:34 -0500

    add Login and signup js files

---
+++ front/src/Components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Context} from '../hookAndContext/context'
+
+const loginContext = useContext(Context)
+
+const Login = () => {
+    return(
+        <form>
+        <h1>Signup</h1>
+        </form>
+    )
+}
+
+export default Login;
---
+++ front/src/Components/Signup.js
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react'
+import {Context} from '../hookAndContext/context'
+
+const signupContext = useContext(Context)
+
+const Signup = () => {
+return(
+    <form>
+    <h1>Signup</h1>
+    </form>
+)
+}
+
+export default Signup;
