c62c1fa28325dd8d7b0b73324ee06bedebedf8ab
= <jacex17@gmail.com>
2019-12-29 18:57:42 -0500

    add city and state to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,12 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    city:{
+        type: String
+    },
+    state: {
+        type: String
+    },
     zipcode: {
         type: String,
         required: true
