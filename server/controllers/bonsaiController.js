c7612e90d362814524e713001530ded405d6c1e7
= <jacex17@gmail.com>
2019-12-30 23:42:19 -0500

    add bonsai controller file

---
+++ server/controllers/bonsaiController.js
@@ -0,0 +1,7 @@
+const Bonsai = require('../models/Bonsai');
+
+module.exports = { 
+    createBonsai(req, res, next) {
+        console.log(req.body)
+    }
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,12 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
+const bonsaiController = require('../controllers/bonsaiController');
 
-router.post('/signup', userController.signUp)
-router.post('/login', userController.logIn)
-router.get('/loggedin', userController.loggedIn)
-router.delete('/logout', userController.logOut)
+router.post('/signup', userController.signUp);
+router.post('/login', userController.logIn);
+router.get('/loggedin', userController.loggedIn);
+router.delete('/logout', userController.logOut);
+router.post('/newBonsai', bonsaiController.createBonsai);
+
 module.exports = router
