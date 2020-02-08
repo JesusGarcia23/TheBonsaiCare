99f048131ab3c9418a168b31f033b9aafab2dd96
= <jacex17@gmail.com>
2020-02-07 22:37:46 -0500

    Add inputs and labels to createbonsai form

--- front/src/Components/CreateBonsai.js
+++ front/src/Components/CreateBonsai.js
@@ -3,7 +3,11 @@ import React from 'react';
 const CreateBonsai = (props) => {
     return(
         <form>
-        
+        <label></label>
+        <input></input>
+
+        <label></label>
+        <input></input>
         </form>
     )
 }
