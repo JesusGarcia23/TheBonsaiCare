d7c2baf2b8afa9fcccea12d8a8176a3f7a2e38bd
= <jacex17@gmail.com>
2020-02-09 23:07:23 -0500

    Add become a carer component to routes

--- front/src/App.js
+++ front/src/App.js
@@ -4,10 +4,12 @@ import {Switch, Route} from 'react-router-dom';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 import CareSignup from './Components/CareSignup';
-import ImageTest from './Components/ImageTest';
 import Dashboard from './Components/Dashboard';
 import CreateBonsai from './Components/CreateBonsai';
+import BecomeCarer from './Components/BecomeCarer';
 import './App.css';
+
+
 const Navbar = lazy(() => import('./Components/Navbar'));
 
 const App = () => {
@@ -21,7 +23,7 @@ const App = () => {
     <Route exact path='/caresignup' component={CareSignup}/>
     <Route exact path='/dashboard' component={Dashboard}/>
     <Route exact path='/createBonsai' component={CreateBonsai}/>
-    <Route exact path='/becomeacarer'></Route>
+    <Route exact path='/becomeacarer' component={BecomeCarer}></Route>
     </Switch>
     
     </Suspense>
